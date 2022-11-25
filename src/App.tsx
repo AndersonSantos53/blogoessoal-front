9907310f2004a6b09e7a53fa5d5344e8e95a34b9
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-11-25 09:37:37 -0300

    Main atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './componentes/estaticos/NavBar/NavBar';
+import Footer from './componentes/estaticos/Fooder/Fooder';
+import Home from './paginas/Home/Home'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <NavBar/>
+    <Home/>
+    <Footer/>
+   </>
   );
 }
 
