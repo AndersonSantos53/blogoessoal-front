77a7dad8627e0741805e3431f372122a9d9534b5
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-11-29 14:40:21 -0300

    Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import NavBar from './componentes/estaticos/NavBar/NavBar';
 import Footer from './componentes/estaticos/Fooder/Fooder';
 import Home from './paginas/Home/Home'
+import Login from './paginas/Login/Login';
 import './App.css';
 
 function App() {
   return (
-   <>
-    <NavBar/>
-    <Home/>
-    <Footer/>
-   </>
+    <Router>
+      <NavBar />
+      <Routes>
+          <Route path="/" element={<Login  />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
