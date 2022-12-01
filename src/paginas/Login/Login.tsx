ba432a574fc51e1c25609305bf8e8f4092303087
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-12-01 16:46:51 -0300

    Main

--- src/paginas/Login/Login.tsx
+++ src/paginas/Login/Login.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import {Box} from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
-import { api } from '../../services/Service';
+import { login } from '../../services/Service';
 import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
@@ -36,7 +36,7 @@ function Login() {
         async function onSubmit(e: ChangeEvent<HTMLFormElement>){
             e.preventDefault();
             try{
-                const resposta = await api.post(`/usuarios/logar`, userLogin)
+                await login(`/usuarios/logar`, userLogin)
                 setToken(resposta.data.token)
 
                 alert('Usuário logado com sucesso!');
