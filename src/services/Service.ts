3522a7841ba1102ecb47c25d9bf6520e7e1771f8
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-12-06 14:14:46 -0300

    Main

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://blogpessoal-65s0.onrender.com'
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
