1c0c101ca87ef6945e2b852c6241f80df281104f
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-12-12 16:03:35 -0300

    Main

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,24 @@ import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
 import CadastroTema from './components/temas/cadastroTema/CadastroTema';
-import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import DeletarPostagem from './components/postagens/deletaPostagem/DeletarPostagem';
-import { Provider } from 'react-redux';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import {Provider} from 'react-redux';
 import store from './store/store';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <Provider store={store}>
-    <Router>
+      <ToastContainer />
+      <Router>
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
         <Routes>
 
           <Route path="/" element={<Login />} />
-
-
 
           <Route path="/login" element={<Login />} />
 
